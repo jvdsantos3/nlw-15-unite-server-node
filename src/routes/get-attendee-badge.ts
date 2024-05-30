cb3b56867f2423bf3608a6aecb8bc1a304304f2f
joao <jv.dossantos012@gmail.com>
2024-05-30 17:45:39 -0300

    add id from badge

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -17,6 +17,7 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         response: {
           200: z.object({
             badge: z.object({
+              id: z.number().int(),
               name: z.string(),
               email: z.string().email(),
               eventTitle: z.string(),
@@ -53,6 +54,7 @@ export async function getAttendeeBadge(app: FastifyInstance) {
 
       return reply.send({
         badge: {
+          id: attendeeId,
           name: attendee.name,
           email: attendee.email,
           eventTitle: attendee.event.title,
